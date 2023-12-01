8e17fa59964428cb85d83b0d5373a952910e5fb9
@DEAD-BILLAH-SHOBUZ <arifbillahmahamudaamena@gmail.com>
2023-12-01 10:47:24 +0600

    database products and products schema edit

--- database/migrations/2023_11_30_092539_create_products.php
+++ database/migrations/2023_11_30_092539_create_products.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title',30);
+            $table->string('short_description',250);
+            $table->string('price',30);
+            $table->tinyInteger('discount',50);
+            $table->string('discount_price',20);
+            $table->string('image',300);
+            $table->tinyInteger('stock',50);
+            $table->double('star',8,2);
+            $table->enum('remark',['yes','no']);
+            // Relation 
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('category_id')->references('id')->on('categories')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreign('brand_id')->references('id')->on('brands')->restrictONDelete()->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
         });
     }
 
--- database/migrations/2023_11_30_092546_create_product_details.php
+++ database/migrations/2023_11_30_092546_create_product_details.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('product_details', function (Blueprint $table) {
             $table->id();
+            $table->string('image1',300);
+            $table->string('image2',300);
+            $table->string('image3',300);
+            $table->string('image4',300);
+            $table->longText('description');
+            $table->string('color');
+            $table->string('size');
+            // Relation 
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('product_details')->restrictOnDelete()->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
             $table->timestamps();
         });
     }
